3f90185beda49780fb97fc537f85278c1bae8408
urmillive <urmillive@gmail.com>
2024-12-29 14:42:45 +0530

    add socket.io and ngx-socket-io dependencies; initialize backend structure with server setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+require('dotenv').config()
+app.use(express.static('public'))
 
-app.use('/', (req, res) => {
-    console.log('Hello World');
-    res.send('Hello World');
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${ process.env.PORT || 3000 }`);
+})
