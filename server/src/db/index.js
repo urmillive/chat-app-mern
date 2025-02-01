888d7359d81bb23966d14c384702704084cb7557
URMIL RUPARELIYA <urmilrupareliya@URMILs-MacBook-Air.local>
2025-02-01 14:27:53 +0530

    Add backend and frontend structure; create sample environment files and initial components

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -3,8 +3,8 @@ import { DB_NAME } from "../contants.js";
 import express from "express";
 
 const app = express()
-
 const connectDB = (async () => {
+
     try {
         const connectionInstance = await mongoose.connect(`${ process.env.MONGO_URL }/${ DB_NAME }`);
         console.log(`MongoDB Connected !! HOST : ${ connectionInstance.connection.host }`);
